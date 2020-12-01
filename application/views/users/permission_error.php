24aabef214296470cf49b4a8d98ee15dca408cab
kuriko22 <kurrko2277gear@docomo.ne.jp>
2020-12-01 09:41:20 +0900

    エラーページと退会ページを作成しました

---
+++ application/views/users/permission_error.php
@@ -0,0 +1,24 @@
+<?php $this->load->view('elements/topmenu'); ?>
+
+<div class="gap">
+    <div class="main-contents container">
+        <div class="shadow-lg p-3 mb-5 bg-white rounded my-5">
+            <img src="/assets/image/1/icon/5fa2c65bbb3d5.png" class="rounded mx-auto d-block" alt="error_image">
+            <h2 class="text-center my-5 font-weight-bolder">会員登録をお願いします。</h2>
+
+            <!-- XXXの欄はJavaScriptで入力内容が変化する -->
+            <h4 class="text-center my-4">XXX　は、<br>
+            会員登録をするとご利用いただけます。</h4>
+
+        <div class="text-center my-4">
+                <a href="/users/regist_user" class="btn btn-success w-25 mx-auto text-center">会員登録</a>
+            </div>
+            <div class="text-center my-4">
+                <a href="/users/login" class="btn btn-primary w-25 mx-auto text-center">ログイン</a>
+            </div>
+            <div class="text-center my-4">
+                <a href="#" class="btn btn-danger w-25 mx-auto text-center">キャンセル</a>
+            </div>
+        </div>
+    </div>
+</div>
--- application/views/users/unsubscribe.php
+++ application/views/users/unsubscribe.php
@@ -1,3 +1,27 @@
-unsubscribe
+<?php $this->load->view('elements/topmenu'); ?>
 
-/* 退会ページ */
+<div class="gap">
+    <div class="main-contents container">
+        <div class="shadow-lg p-3 mb-5 bg-white rounded my-5">
+            <h1 class="text-center my-5 font-weight-bold">退会</h1>
+            <h5 class="text-center">このページから退会ができます。</h5>
+            <h5 class="text-center">退会すると、登録作品も削除され、</h5>
+            <h5 class="text-center">購入した商品も閲覧できなくなります。</h5>
+
+            <!-- 退会理由入力欄 -->
+            <form>
+                <div class="form-row text-left justify-content-center my-5">
+                    <div class="col-sm-5">
+                        <div class="form-group">
+                            <textarea rows="7" id="textarea1" class="form-control" placeholder="退会理由を教えてください" ></textarea>
+                        </div>
+                    </div>
+                </div>
+            </form>
+
+            <div class="text-center my-5">
+                <a href="/top/index" class="btn btn-primary w-25 text-center mx-auto">退会する</a>
+            </div>
+        </div>
+    </div>
+</div>
