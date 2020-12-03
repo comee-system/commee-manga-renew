2cd9fbed45a34d34ed7147ab160834f084114c1d
fukaur <omiya313@gmail.com>
2020-12-03 17:57:28 +0900

    '12/03指摘事項修正、クリエイターページ作成等'

--- application/views/users/update_email.php
+++ application/views/users/update_email.php
@@ -1,11 +1,11 @@
 <?php $this->load->view('elements/topmenu'); ?>
 
 <div class="main-contents container">
-    <div class="alert alert-secondary w-100 mx-auto text-center" role="alert">アカウント設定【メールアドレスの変更】</div>
+    <div class="alert alert-secondary w-100 mx-auto text-center p-4" role="alert">アカウント設定【メールアドレスの変更】</div>
     
     
     <div class="shadow-lg p-3 mb-5 bg-white rounded">
-        <form>
+        <form method="POST" action="#">
             <div class="text-center my-5">
                 <div class="form-row text-left justify-content-center ">
                 <label class="col-sm-8 col-sm-offset-2"> 新しいメールアドレス</label>
@@ -21,27 +21,7 @@
                 <a href=/users/settings><button type="button" class="btn btn-danger w-25 mr-5">キャンセル</button></a>
                 <button type="submit" formaction="/users/settings" class="btn btn-success w-25 ml-5">保存</button>
             </div>
-            <br>
         </form>
     </div>
 
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
