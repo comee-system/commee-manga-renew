6531fdc1e7312779ba19cc98836412c664256d1b
fukaur <omiya313@gmail.com>
2020-12-07 09:00:58 +0900

    mypage(通知)更新、(月額会員)作成中

--- application/views/mypage/notice.php
+++ application/views/mypage/notice.php
@@ -4,114 +4,92 @@
 
     <!-- 通知枠 -->
     <div class="shadow-lg mb-5 bg-white rounded">
-        <h3 class="text-dark p-2 pl-5 my-4">通知</h3>
+        <h3 class="text-center text-dark pl-5 pt-4">通知</h3>
 
             <div class="d-flex flex-column bd-highlight">
 
-
-
-
-
-                <!-- 通知例(Like) -->
-                <div class="bg-light mt-3">
-                    <div class="media position-relative ml-3">
-                        <img src="/assets/image/1/icon/5fa2c65bbb3d5.png" class="img-thumbnail rounded-circle" width="100" height="100"><img src="/assets/image/5/announce/49/icon.png" class="img-thumbnail rounded-circle" width="100" height="100"><img src="/assets/image/1/icon/5fa2c65bbb3d5.png" class="img-thumbnail rounded-circle" width="100" height="100">
-                        <div class="media-body">
-                            <h5 class="py-5 d-flex align-items-center justify-content-center text-dark font-weight-bold">○○さんと他ｘ人がXXXにLikeしました！</h5>
-                            <a href="/" class="stretched-link"></a>
+              　<!-- 通知例(Like、複数人の場合) -->
+                <div class="bg-light  position-relative">
+                    <div class="media ml-3">
+                        <div class="row col-sm">
+                            <div class="col-md-6 my-auto d-flex justify-content-center">
+                                    <a href="#"><img src="/assets/image/1/icon/5fa2c65bbb3d5.png" class="img-thumbnail rounded-circle mr-3" width="100" height="100"></a><a href="#"><img src="/assets/image/1/icon/5fa2c65bbb3d5.png" class="mr-3 img-thumbnail rounded-circle" width="100" height="100"></a><a href="#"><img src="/assets/image/1/icon/5fa2c65bbb3d5.png" class="mr-3 img-thumbnail rounded-circle" width="100" height="100"></a>
+                            </div>
+                            <div class="media-body">
+                                <h5 class="py-5 d-flex align-items-center justify-content-center text-dark font-weight-bold">○○さんと他ｘ人がXXXにLikeしました！</h5>
+                            </div>   
                         </div>
                     </div>
                 </div>
+                <div class="pb-3 bg-white"></div>
 
-
-                <!-- 通知例(有料記事の購入) -->
-                <div class="bg-light mt-3">
-                    <div class="media position-relative ml-3">
-                        <img src="/assets/image/5/announce/49/icon.png" class="img-thumbnail rounded-circle" width="120" height="120">
+                 <!-- 通知例(投稿) -->
+                 <div class="bg-light position-relative">
+                    <div class="media">
+                        <div class="row media-body col-md">
+                            <div class="col-md-6 my-auto d-flex justify-content-center">
+                                <img src="/assets/image/5/announce/49/icon.png" class="img-thumbnail rounded-circle" width="100" height="100">
+                            </div>
                         <div class="media-body">
-                            <h5 class="py-5 d-flex align-items-center justify-content-center text-dark font-weight-bold">○○さんがXXXを購入しました！</h5>
-                            <a href="/" class="stretched-link"></a>
+                            <h5 class="py-5 d-flex justify-content-center text-dark font-weight-bold">○○さんがXXXを投稿しました！</h5>
+                            <a href="#" class="stretched-link"></a>
                         </div>
                     </div>
                 </div>
+                <div class="pb-3 bg-white"></div>
 
                 <!-- 通知例(支援) -->
-                <div class="bg-light mt-3">
-                    <div class="media position-relative ml-3">
-                        <img src="/assets/image/1/icon/5fa2c65bbb3d5.png" class="img-thumbnail rounded-circle" width="120" height="120">
+                <div class="bg-light position-relative">
+                    <div class="media">
+                        <div class="row media-body col-md">
+                            <div class="col-md-6 my-auto d-flex justify-content-center"> 
+                                <img src="/assets/image/1/icon/5fa2c65bbb3d5.png" class="img-thumbnail rounded-circle" width="100" height="100">
+                            </div>
                         <div class="media-body">
-                            <h5 class="py-5 d-flex align-items-center justify-content-center text-dark font-weight-bold">○○さんがあなたを支援しました！</h5>
-                            <a href="/" class="stretched-link"></a>
+                            <h5 class="py-5 d-flex justify-content-center text-dark font-weight-bold">○○さんがあなたを支援しました！</h5>
+                            <a href="#" class="stretched-link"></a>
                         </div>
                     </div>
                 </div>
+                <div class="pb-3 bg-white"></div>
 
-                <!-- 通知例(投稿) -->
-                <div class="bg-light mt-3">
-                    <div class="media position-relative ml-3">
-                        <img src="/assets/image/5/announce/49/icon.png" class="img-thumbnail rounded-circle" width="120" height="120">
+
+
+                <!-- 通知例・案１(有料記事の購入―スマホ版だと縦表示に変更) -->
+                <div class="bg-light position-relative">
+                    <div class="media">
+                        <div class="row media-body col-md">
+                            <div class="col-md-6 my-auto d-flex justify-content-center">
+                                 <img src="/assets/image/5/announce/49/icon.png" class="img-thumbnail rounded-circle" width="100" height="100">
+                            </div>
                         <div class="media-body">
-                            <h5 class="py-5 d-flex align-items-center justify-content-center text-dark font-weight-bold">○○さんがXXXを投稿しました！</h5>
-                            <a href="/" class="stretched-link"></a>
+                            <h5 class="py-5 d-flex justify-content-center text-dark font-weight-bold">○○さんがXXXを購入しました！</h5>
+                            <a href="#" class="stretched-link"></a>
                         </div>
                     </div>
                 </div>
+                <div class="pb-3 bg-white"></div>
 
-                
-
-                
-
-
-
-
-
-                <div class="row">
-                    <div class="col-sm">
-                    One of three columns
-                    </div>
-                    <div class="col-sm">
-                    One of three columns
-                    </div>
-                </div>
-
-
-                　　<!-- 通知例(支援) -->
-                <div class="p-2 bg-light mt-3">
-                    <div class="media position-relative p-2 bd-highlight d-flex">
-                        <div class="row">
-                            <div class="col-sm"><img src="/assets/image/1/icon/5fa2c65bbb3d5.png" class="mr-3 img-thumbnail rounded-circle" width="120" height="120"><img src="/assets/image/1/icon/5fa2c65bbb3d5.png" class="mr-3 img-thumbnail rounded-circle" width="120" height="120"><img src="/assets/image/1/icon/5fa2c65bbb3d5.png" class="mr-3 img-thumbnail rounded-circle" width="120" height="120"></div>
-                            <div class="media-body col-sm"><h5 class="py-5 d-flex align-items-center justify-content-center">○○さんがXXXに支援しました。</h5></div>
-                           
+                <!-- 通知例・案２(有料記事の購入―スマホ版でも横表示のまま)-->
+                <div class="bg-light position-relative">
+                    <div class="media">
+                        <div class="media-body my-auto d-flex justify-content-center">
+                            <img src="/assets/image/5/announce/49/icon.png" class="img-thumbnail rounded-circle" width="100" height="100">
+                        </div>
+                        <div class="media-body">
+                            <h5 class="py-5 d-flex justify-content-center text-dark font-weight-bold">○○さんがXXXを購入しました！</h5>
+                            <a href="#" class="stretched-link"></a>
                         </div>
                     </div>
                 </div>
-
-
-
-
-
-                <div class="p-2 bd-highlight">Flexアイテム2</div>
-                <div class="p-2 bd-highlight">Flexアイテム3</div>
+                <div class="pb-3 bg-white"></div>
+               
             </div>
 
-        
-
-
-
-
-
-
-
-
-
-
-
-        aaaaaaaaaaa
-
-            <nav class="navbar navbar-light bg-light pb-0">
-            <h5 class="">以前の通知を表示する</h5>
-            <a href="/" class="stretched-link"></a>
-            </nav>
+            <!-- 以前の通知を表示（スクロールで読み込む） -->
+            <div class="text-white bg-dark text-center">
+                <h5>以前の通知を表示する</h5>
+            </div>
         
     </div>
 </div>
