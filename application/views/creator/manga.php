2f8cb3b633c021ee2203907e2d230d69a6001d53
kuriko22 <kurrko2277gear@docomo.ne.jp>
2020-12-10 17:17:10 +0900

    manga.phpを更新しました。

--- application/views/creator/manga.php
+++ application/views/creator/manga.php
@@ -1,21 +1,16 @@
-<?php $this->load->view('elements/topmenu'); ?>
 
-    <div class="main-contents container">
-
-        <!-- 作品表示と作品情報を枠で囲んでグループ化する -->
-        <div class="border">
 
             <!-- 作品表示 -->
-            <div class="alert alert-dark" role="alert">
-                <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+            <div class="alert alert-dark mt-3" role="alert">
+                <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel" data-interval="false">
                     <div class="carousel-inner">
-                        <div class="carousel-item active">
+                        <div class="carousel-item active d-flex justify-content-around">
                             <img src="/assets/image/5/detail/18/lists/s_5fa4eba8a7e4a.jpg" class="d-block" alt="作品ページ➀">
                         </div>
-                        <div class="carousel-item">
+                        <div class="carousel-item d-flex justify-content-around">
                             <img src="/assets/image/5/detail/18/lists/s_5fa4eba8a87bb.jpg" class="d-block" alt="作品ページ➁">
                         </div>
-                        <div class="carousel-item">
+                        <div class="carousel-item d-flex justify-content-around">
                             <img src="/assets/image/5/detail/18/lists/s_5fa4eba8aaa1b.jpg" class="d-block" alt="作品ページ➂">
                         </div>
                     </div>
@@ -31,6 +26,7 @@
                 </div>
             </div>
 
+        <div class="container">
 
             <!-- 作品情報表示部分 -->
             <div class="d-flex justify-content-between bd-highlight">
@@ -45,71 +41,172 @@
 
             <hr>
 
-            <div class="row">
-                <div class="col-sm-6">
-                    <div class="d-flex justify-content-start bd-highlight mb-1">
-                        <div class="p-2 bd-highlight text-left mt-1">
-                            <p class="my-0 mx-2">ユーザーネーム</p>
-                            <p class="mt-1 mb-3 mx-2">投稿日時XXXX/XX/XX&nbsp;XX:XX</p>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <div class="d-flex justify-content-start bd-highlight mb-1">
+                            <div class="p-2 bd-highlight text-left mt-1">
+                                <p class="my-0">ユーザーネーム</p>
+                                <p class="mt-1 mb-3">投稿日時XXXX/XX/XX&nbsp;XX:XX</p>
+                            </div>
                         </div>
-                        <div class="p-2 bd-highlight mt-3 mx-1">
+                    </div>
+                    <div class="col-sm-9">
+                        <div class="float-left mt-3 mb-2">
                             <a class="btn btn-success" href="#" role="button">編集する</a>
                         </div>
-                    </div>
-                </div>
-                <div class="col-sm-6">
-                    <div class="d-flex justify-content-end bd-highlight mb-1">
-                        <div class="p-2 bd-highlight mt-2">
-                            <a href=""><i class="fab fa-facebook-square fa-3x" style="color:blue;"></i><a>
+                        <div class="float-right mt-3 mb-2">
+                            <a href=""><i class="fab fa-facebook-square fa-3x mx-1" style="color:blue;"></i><a>
+                            <a href=""><i class="fab fa-line fa-3x mx-1" style="color:green;"></i><a>
+                            <a href=""><i class="fab fa-twitter-square fa-3x mx-1" style="color:skyblue;"></i><a>
                         </div>
-                        <div class="p-2 bd-highlight mt-2">
-                            <a href=""><i class="fab fa-line fa-3x" style="color:green;"></i><a>
-                        </div>
-                        <div class="p-2 bd-highlight mt-2">
-                            <a href=""><i class="fab fa-twitter-square fa-3x" style="color:skyblue;"></i><a>
-                        </div>
-                    </div>
+                    </div>    
                 </div>
             </div>
 
         </div>
 
+    <hr>
+
+    <div class="container">
+
         <!-- 支援ボタン -->
-        <div class="alert alert-success my-3" role="alert">
+        <div class="alert alert-success my-4" role="alert">
             <h4 class="alert-heading text-center">あなたの応援が作品を描く支えになります。</h4>
             <hr>
             <div class="text-center">
-                <button type="button" class="btn btn-success btn-lg">
-                    <h5 class="">支援する</h5>
+                <button type="button" class="btn-success btn-lg">
+                    <h5 class="mt-2">支援する</h5>
                 </button>
             </div>
         </div>
 
         <!-- 報告ボタンをrowで横並びにするか？ -->
-        <button type="button" class="btn btn-secondary">報告する</button>
+        <div class="d-flex flex-row-reverse bd-highlight">
+            <button type="button" class="btn btn-danger btn-sm">報告する</button>
+        </div>
+    
 
         <!-- タグ表示欄(＃xxx) -->
-        <div class="container">
-            <div class="row">
-                <div class="col-2">
-                    <button type="button" class="ncss btn-outline-dark">#XXX</button>
-                </div>
-                <div class="col-2">
-                    <button type="button" class="ncss btn-outline-dark">#XXXX</button>
-                </div>
-                <div class="col-2">
-                    <button type="button" class="ncss btn-outline-dark">#XXX</button>
-                </div>
-                <div class="col-2">
-                    <button type="button" class="ncss btn-outline-dark">#XX</button>
-                </div>
-                <div class="col-2">
-                    <button type="button" class="ncss btn-outline-dark">#XXX</button>
-                </div>
-                <div class="col-2">
-                    <button type="button" class="ncss btn-outline-dark">#XXXXX</button>
+        <div class="row">
+            <div class="col-10 offset-1">
+                <div class="d-flex flex-wrap my-3">
+                    <button type="button" class="btn btn-outline-dark m-1">#XXX</button>
+                    <button type="button" class="btn btn-outline-dark m-1">#XXXXX</button>
+                    <button type="button" class="btn btn-outline-dark m-1">#XXX</button>
+                    <button type="button" class="btn btn-outline-dark m-1">#XX</button>
+                    <button type="button" class="btn btn-outline-dark m-1">#XXX</button>
                 </div>
             </div>
         </div>
+
+        <!-- 連載＋フォロー表示欄 -->
+        <div class="card my-3">
+            <img src="/assets/image/5/detail/16/thum/5f6ffef48c677.jpg" class="card-img-top" alt="ヘッダー画像">
+            <div class="card-body">
+                <div class="card-text">
+                    <div class="d-flex justify-content-between mt-1">
+                        <div class="ml-2">
+                            <h4>連載タイトル</h4>
+                            <p>作品数</p>
+                        </div>
+                        <div class="mr-2">
+                            <button type="button" class="btn btn-success">フォロー中</button>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-10 offset-1 text-break">
+                            <p>XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX(作品の説明)</p>
+                        </div>
+                    </div>
+                    <div class="text-right mr-3">
+                        <h5>月額XX円</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- クリエイター＋フォロー表示欄 -->
+        <div class="card my-3">
+            <div class="card-body">
+                <div class="row card-text">
+                    <div class="col-md-3 mt-3 text-center">
+                        <img src="/assets/image/5/announce/49/xs_5f6f3e6306856.jpg" class="rounded-circle" alt="アイコン">
+                    </div>
+                    <div class="col-md-9">
+                        <div class="d-flex bd-highlight">
+                            <div class="p-2 flex-fill bd-highlight">
+                                <h5 class="text-nowrap">ユーザーネーム</h5>
+                            </div>
+                            <div class="p-2 flex-fill bd-highlight">
+                                <p class="text-nowrap">フォロワー数</p>
+                            </div>
+                            <div class="p-2 flex-fill bd-highlight">
+                                <button type="button" class="btn btn-success mr-1">フォロー</button> 
+                            </div>
+                        </div>
+                        <p>連載の説明XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        
+        <!-- 連載作品一覧 -->
+        <div class="border">
+
+            <div class="border p-3">
+                <div class="row no-gutters bg-light position-relative">
+                    <div class="col-md-6 mb-md-0 p-md-4">
+                        <img src="/assets/image/5/detail/17/thum/s_5f719079b5045.jpg" class="w-100" alt="画像/サムネイル">
+                    </div>
+                    <div class="col-md-6 position-static p-4 pl-md-0">
+                        <h3 class="text-left"><a href="/creator/manga?id={id}" class="text-dark">第1話：XXXXXXXXXXXXXXX</a></h4>
+                        <h5 class="mt-3"><a href="/creator?id={id}" class="text-dark">ユーザーネーム</a></h5>
+                        <h4 class="mt-1"><a href="/creator/serial?id={id}" class="text-dark">地縛少年花子くん</a></h4>
+                        <h5 class="mt-1">2020/12/31 0:00</h5>
+                        <h4 class="mt-3"><i class="far fa-heart">100</i></h4>
+                    </div>
+                </div>
+            </div>
+
+            <div class="border p-3">
+                <div class="row no-gutters bg-light position-relative">
+                    <div class="col-md-6 mb-md-0 p-md-4">
+                        <img src="/assets/image/5/detail/17/thum/s_5f719079b5045.jpg" class="w-100" alt="画像/サムネイル">
+                    </div>
+                    <div class="col-md-6 position-static p-4 pl-md-0">
+                        <h3 class="text-left"><a href="/creator/manga?id={id}" class="text-dark">第2話：XXXXXXXXXXXXXXX</a></h4>
+                        <h5 class="mt-3"><a href="/creator?id={id}" class="text-dark">ユーザーネーム</a></h5>
+                        <h4 class="mt-1"><a href="/creator/serial?id={id}" class="text-dark">地縛少年花子くん</a></h4>
+                        <h5 class="mt-1">2020/12/31 0:00</h5>
+                        <h4 class="mt-3"><i class="far fa-heart">100</i></h4>
+                    </div>
+                </div>
+            </div>
+
+            <div class="border p-3">
+                <div class="row no-gutters bg-light position-relative">
+                    <div class="col-md-6 mb-md-0 p-md-4">
+                        <img src="/assets/image/5/detail/17/thum/s_5f719079b5045.jpg" class="w-100" alt="画像/サムネイル">
+                    </div>
+                    <div class="col-md-6 position-static p-4 pl-md-0">
+                        <h3 class="text-left"><a href="/creator/manga?id={id}" class="text-dark">第3話：XXXXXXXXXXXXXXX</a></h4>
+                        <h5 class="mt-3"><a href="/creator?id={id}" class="text-dark">ユーザーネーム</a></h5>
+                        <h4 class="mt-1"><a href="/creator/serial?id={id}" class="text-dark">地縛少年花子くん</a></h4>
+                        <h5 class="mt-1">2020/12/31 0:00</h5>
+                        <h4 class="mt-3"><i class="far fa-heart">100</i></h4>
+                    </div>
+                </div>
+            </div>
+
+            <!-- もっと見るボタン -->
+            <div class="text-center my-3">
+                <button type="button" class="ncss btn-success btn-lg">もっと見る</button>
+            </div>
+        
     
     </div>
